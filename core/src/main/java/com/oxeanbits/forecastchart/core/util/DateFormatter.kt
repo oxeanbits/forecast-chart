e023301e6a39274c344431b32ef4af035e67010e
rodigui <rodrigo_fpinho@hotmail.com>
2019-11-22 16:29:48 -0300

    Fixing timestamp conversion bug

--- core/src/main/java/com/oxeanbits/forecastchart/core/util/DateFormatter.kt
+++ core/src/main/java/com/oxeanbits/forecastchart/core/util/DateFormatter.kt
@@ -4,9 +4,10 @@ import java.text.SimpleDateFormat
 import java.util.Date
 
 object DateFormatter{
+    const val ONE_DAY = 86400
     fun timestampToDate(timestamp: Long): String{
         val mDate = Date()
-        mDate.time = (timestamp.times(1000))
+        mDate.time = timestamp.times(1000) + ONE_DAY
         return SimpleDateFormat("yyyy-MM-dd").format(mDate)
     }
 }
