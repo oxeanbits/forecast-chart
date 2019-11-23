e6e736f49223eb2de1c4fc25c93d78f3f7f17314
rodigui <rodrigo_fpinho@hotmail.com>
2019-11-22 22:33:22 -0300

    Implementing new date formatter to fix timezone bug.

--- core/src/main/java/com/oxeanbits/forecastchart/core/util/DateFormatter.kt
+++ core/src/main/java/com/oxeanbits/forecastchart/core/util/DateFormatter.kt
@@ -1,13 +1,22 @@
 package com.oxeanbits.forecastchart.core.util
 
-import java.text.SimpleDateFormat
-import java.util.Date
+import org.threeten.bp.Instant
+import org.threeten.bp.LocalDate
+import org.threeten.bp.LocalDateTime
+import org.threeten.bp.ZoneOffset
+import org.threeten.bp.format.DateTimeFormatter
+
 
 object DateFormatter{
-    const val ONE_DAY = 86400
+
+    fun stringToTimestamp(date: String): Float{
+        return LocalDate.parse(date).atStartOfDay()
+            .toInstant(ZoneOffset.UTC).epochSecond.toFloat()
+    }
+
     fun timestampToDate(timestamp: Long): String{
-        val mDate = Date()
-        mDate.time = timestamp.times(1000) + ONE_DAY
-        return SimpleDateFormat("yyyy-MM-dd").format(mDate)
+        val mDate = LocalDateTime.ofInstant(Instant.ofEpochSecond(timestamp), ZoneOffset.UTC)
+        val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+        return mDate.format(formatter)
     }
 }
