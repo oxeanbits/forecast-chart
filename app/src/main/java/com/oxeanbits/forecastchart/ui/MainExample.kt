a9b21a06d508feaab7dbe23e9ee48a878ba64702
rodigui <rodrigo_fpinho@hotmail.com>
2019-11-29 14:00:16 -0300

    Implementing new examples

--- app/src/main/java/com/oxeanbits/forecastchart/ui/MainExample.kt
+++ app/src/main/java/com/oxeanbits/forecastchart/ui/MainExample.kt
@@ -2,12 +2,15 @@ package com.oxeanbits.forecastchart.ui
 
 import android.os.Bundle
 import android.view.View
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import com.jakewharton.threetenabp.AndroidThreeTen
 import com.oxeanbits.forecastchart.core.ui.component.forecastChartComponent
 import com.oxeanbits.forecastchart.util.SetupChartExample
 import trikita.anvil.BaseDSL.MATCH
 import trikita.anvil.BaseDSL.size
+import trikita.anvil.DSL.linearLayout
+import trikita.anvil.DSL.orientation
 import trikita.anvil.RenderableView
 
 class MainExample : AppCompatActivity() {
@@ -21,12 +24,30 @@ class MainExample : AppCompatActivity() {
     private fun getView(): View {
         return object : RenderableView(this) {
             override fun view() {
-                forecastChartComponent{
-                    size(MATCH, 350)
-                    loadForecastChart(
-                        SetupChartExample.getExpectedObj(),
-                        SetupChartExample.getActualObj()
-                    )
+                linearLayout{
+                    size(MATCH, MATCH)
+                    orientation(LinearLayout.VERTICAL)
+
+                    forecastChartComponent{
+                        size(MATCH, 350)
+                        loadForecastChart(
+                            SetupChartExample.getExpectedObj(),
+                            SetupChartExample.getActualObj(),
+                            SetupChartExample.getForecastedObj(),
+                            SetupChartExample.getEndDateObj(),
+                            "m³"
+                        )
+                    }
+
+                    forecastChartComponent {
+                        size(MATCH, 350)
+                        loadForecastChart(
+                            SetupChartExample.getActualObj(),
+                            SetupChartExample.getForecastedObj(),
+                            SetupChartExample.getEndDateObj(),
+                            "m³"
+                        )
+                    }
                 }
             }
         }
