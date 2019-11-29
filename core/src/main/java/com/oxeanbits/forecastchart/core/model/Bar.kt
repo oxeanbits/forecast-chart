26c32c8ec07d7b46e7277e4244c5ff1dfd0f314d
rodigui <rodrigo_fpinho@hotmail.com>
2019-11-29 08:47:13 -0300

    Refactoring models

--- core/src/main/java/com/oxeanbits/forecastchart/core/model/Bar.kt
+++ core/src/main/java/com/oxeanbits/forecastchart/core/model/Bar.kt
@@ -5,6 +5,5 @@ import com.github.mikephil.charting.data.BarEntry
 data class Bar (
     val values: ArrayList<BarEntry>,
     val label: String,
-    val color: Int,
-    val unity: String
+    val color: Int
 )
--- core/src/main/java/com/oxeanbits/forecastchart/core/model/Line.kt
+++ core/src/main/java/com/oxeanbits/forecastchart/core/model/Line.kt
@@ -6,5 +6,5 @@ data class Line (
     val values: ArrayList<Entry>,
     val label: String,
     val color: Int,
-    val unity: String
+    val forecasted: Boolean
 )
